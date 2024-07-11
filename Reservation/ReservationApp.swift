47c5e527d9c08419cf46008ccec132fc9b832304
Craig Fisher <craig.fisher@includedhealth.com>
2024-07-11 09:41:56 -0700

    Initial Commit

---
+++ Reservation/ReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationApp.swift
+//  Reservation
+//
+//  Created by Craig Fisher on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
